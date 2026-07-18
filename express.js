49d64f2c2d185196f46c4c3d11203185a75b4838
Ashutosh Tiwari <ashutosh122002@gmail.com>
2026-07-18 13:13:13 +0530

    Intial commit

---
+++ express.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const database = {
+    Ashutosh :{password:"12345"},
+    Rahul :{password:"34567"}
+};
+
+function show_profile (req, res){
+    res.send ("Your profile")
+};
+
+function login(req, res){
+   
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!database[username]){
+        return res.send ("username not found")
+    };
+
+    if (database [username].password === password){
+        return res.send("Welcome to jinstagram")
+    };
+
+    return res.send("wrong password");
+
+};
+
+app.get("/profile", show_profile);
+app.post("/login", login);
+
+app.listen(3000, ()=> {
+    console.log("Server working live on https//:localhost3000")
+});
+
