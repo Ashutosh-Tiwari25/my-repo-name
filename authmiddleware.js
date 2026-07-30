b2bab0d9cfa6e2d0f1a779e1e1885aac9c7c395d
Ashutosh Tiwari <ashutosh122002@gmail.com>
2026-07-30 09:36:35 +0530

    function that verifies jwt token

---
+++ authmiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const jwt_secret = "youe_secret_key";
+
+
+function verify_token(req,res,next){
+
+const authHeader= req.body.authenticator;
+
+if(!authHeader){
+    return res.send("no tokenprovided");
+}
+
+const token  = authHeader.split("")[1];
+
+try {
+    const decoded = jwt.verify(token, jwt_secret);
+    req.user= decoded;
+    next();  
+
+} catch (error) {
+    return res.send("Invalid token")
+    
+};
+}
+
+module.exports= {verify_token, jwt_secret};
