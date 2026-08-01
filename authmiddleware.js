b45043894c951b92d1555fe83092bd6bf1079218
Ashutosh-Tiwari25 <113596871+Ashutosh-Tiwari25@users.noreply.github.com>
2026-08-02 00:19:54 +0530

    Fix token verification header and clean up code

--- authmiddleware.js
+++ authmiddleware.js
@@ -1,26 +1,19 @@
 const jwt = require("jsonwebtoken");
 const jwt_secret = "your_secret_key";
 
-
 function verify_token(req,res,next){
-
-const authHeader= req.headers.authenticator;
+const authHeader= req.headers.authorization;
 
 if(!authHeader){
     return res.send("no token provided");
 }
-
 const token  = authHeader.split(" ")[1];
-
 try {
     const decoded = jwt.verify(token, jwt_secret);
     req.user= decoded;
     next();  
-
 } catch (error) {
     return res.send("Invalid token")
-    
 };
 };
-
 module.exports= {verify_token, jwt_secret};
