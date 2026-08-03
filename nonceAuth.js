a64769738709a64852339ea47abd696a8dbe8add
Ashutosh Tiwari <ashutosh122002@gmail.com>
2026-08-03 14:18:58 +0530

    nonce

--- nonceAuth.js
+++ nonceAuth.js
@@ -7,13 +7,15 @@ app.use(express.json());
 
 const database = {};
 const nonceStore = {};
+const nonce_expiry = 5 * 60 * 1000;  // valid for only 5 minutes 
 
 function get_message (req,res){
     const address = req.body.address;
 
     const nonce = Math.floor(Math.random()* 1000000)+ "-" + Date.now();
+    const expiresAt = Date.now() + nonce_expiry;
 
-    nonceStore[address] = nonce;
+    nonceStore[address] = {nonce: nonce, expiresAt: expiresAt};
 
     const message = `Sign this message to log in: ${nonce}`;
     return res.json({message});
@@ -29,21 +31,34 @@ function verifySignature(req,res){
     if (!storednonce){
         return res.send("No nonce found. Kindly, login again");
     }
+
+    if(Date.now()> nonceStore.expiresAt){
+        delete nonceStore[address];
+        return res.send("Nonce expired, kindly login again");
+    }
+
     if (message.indexOf(storednonce)=== -1){
         return res.send("Nonce do not match. Possibly a replay attack");
     }
 
-    const recoveredAddress = ethers.verifyAuthorization(message, signature);
+    try{
+    const recoveredAddress = ethers.verifyMessage(message, signature);
+    } catch(error){
+        console.log("error verifying singature:", error.message);
+        return res.send("Invalid signature");
+    }
+
     if (recoveredAddress.toLowerCase() === address.toLowerCase()){
 
         delete nonceStore[address];
 
         const token = jwt.sign({address: address}, jwt_secret, {expiresIn: "1h"})
-        return res.send("signature verification failed");
+        return res.json({ message: "Wallet verified", token: token });
     }
+    return res.send("Signature Verification failed");
 }
 
-app.get("/web3/message", get_message);
+app.post("/web3/message", get_message);
 app.post("/web3/verify", verifySignature);
 
 app.listen(3000, ()=> {
