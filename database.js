454845ec1a4a70261d34bba1e43b5efbbe6cff28
Ashutosh Tiwari <ashutosh122002@gmail.com>
2026-08-06 08:50:08 +0530

    new table added

--- database.js
+++ database.js
@@ -1,12 +1,23 @@
-const Database = require ("better-sqlite3");
+const Database = require("better-sqlite3");
+
 const db = new Database("users.db");
 
 db.prepare(`
-    CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    password TEXT
-     )
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
 `).run();
 
-module.exports = {db};
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS transfers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        from_address TEXT,
+        to_address TEXT,
+        value TEXT,
+        tx_hash TEXT
+    )
+`).run();
+
+module.exports = db;
